ecfb188b7816f9196b85e35b2403e4081142a784
imapibarrero <ipedroanto@gmail.com>
2023-12-04 17:04:23 -0300

    Tarea Funciones para cargar Array

---
+++ mostar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Productos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container mt-5">
+    <h2>Listado de Productos</h2>
+    <div class="row">
+
+        <?php 
+        include 'productos.php';
+        foreach (datos_productos() as $producto): ?>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
+                        <p class="card-text">Precio: $<?php echo $producto['precio']; ?></p>
+                        <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ productos.php
@@ -0,0 +1,47 @@
+
+<?php
+
+function datos_productos(){
+
+    return [
+        [
+            'id'=>'1',
+            'nombre'=> 'Zapatos_1',
+            'precio'=> 12000
+
+        ],
+        [
+            'id'=>'2',
+            'nombre'=> 'Zapatos_2',
+            'precio'=> 12500
+
+        ],
+        [
+            'id'=>'3',
+            'nombre'=> 'Zapatos_3',
+            'precio'=> 13000
+
+        ],
+        
+        [
+            'id'=>'4',
+            'nombre'=> 'Zapatos_4',
+            'precio'=> 13500
+
+        ],
+        [
+            'id'=>'5',
+            'nombre'=> 'Zapatos_5',
+            'precio'=> 14000
+
+        ],
+    ];
+
+    
+}
+
+
+
+
+?>
+
